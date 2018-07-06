5cfd9dbfea1787bf31a6bfab834f6131d388eea2
gcazaciuc <gcazaciuc@yahoo.com>
2018-07-06 15:44:37 +0300

    Enable firebase on gatsby starter

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Exapp',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Exapp.IO</h1>
+    <p>Empowering social good</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
