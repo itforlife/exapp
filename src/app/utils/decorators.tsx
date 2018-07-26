5493dc6da105ecb17c37c171863bef8406c8ac62
gcazaciuc <gcazaciuc@yahoo.com>
2018-07-26 11:19:45 +0300

    Fix observer on components

--- src/app/utils/decorators.tsx
+++ src/app/utils/decorators.tsx
@@ -5,22 +5,12 @@ import { translate } from 'react-i18next'
 import ApplicationStore from '../modules/layout/ApplicationStore'
 
 export interface IInjectedProps {
-  appStore: typeof ApplicationStore
-  router: RouterProps
-  t: (strPath: string) => string
+    appStore: typeof ApplicationStore
+    router: RouterProps
+    t: (strPath: string) => string
 }
 export const comp = <P extends IInjectedProps>(
-  Comp: React.ComponentType<P>
+    Comp: React.ComponentType<P>
 ) => {
-  const fn = (props: P) => {
-    return (
-      <Comp
-        appStore={props.appStore}
-        router={props.router}
-        t={props.t}
-        {...props}
-      />
-    )
-  }
-  return withRouter(translate(['data'])(inject('appStore')(observer(fn))))
+    return withRouter(translate(['data'])(inject('appStore')(observer(Comp))))
 }
